478033472c56d38a8c13feddef946cc560cabbf9
BautistaPicca <bautistapiccarenaudo@gmail.com>
2025-05-13 20:37:28 -0300

    chore: setup finished
    + Reloader
    + SQLite
    + Logger
    
    * Basic User model
    
    Ready to use

---
+++ app/api/server.rb
@@ -0,0 +1,26 @@
+require 'bundler/setup'
+require 'sinatra/base'
+require 'sinatra/reloader' if Sinatra::Base.environment == :development
+require 'logger'
+require 'active_record'
+require 'sinatra/activerecord'
+
+require_relative 'models/user'
+
+class App < Sinatra::Application
+  configure :development do
+    enable :logging
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::DEBUG if development?
+    set :logger, logger
+
+    register Sinatra::Reloader
+    after_reload do
+      logger.info 'Reloading...'
+    end
+  end
+
+  get '/' do
+    "Welcome"
+  end
+end
