e45cb487ce8c920754415428b9b2a85e4e6bd37e
BautistaPicca <bautistapiccarenaudo@gmail.com>
2025-05-18 05:10:47 -0300

    fix: wallet_member relations

--- app/api/models/wallet_member.rb
+++ app/api/models/wallet_member.rb
@@ -1,2 +1,16 @@
 class WalletMember < ActiveRecord::Base
+  belongs_to :user, class_name: 'User', foreign_key: 'user_dni', primary_key: 'dni'
+  belongs_to :wallet, class_name: 'Wallet', foreign_key: 'wallet_cvu', primary_key: 'cvu'
+
+  validate :must_be_owner_or_shared_wallet
+
+  private
+
+  def must_be_owner_or_shared_wallet
+    return if wallet.nil? || user.nil?
+
+    if wallet.type != 'shared' && wallet.dni_owner != user.dni
+      errors.add(:user, 'can only be added if they are the owner or the wallet is shared')
+    end
+  end
 end
